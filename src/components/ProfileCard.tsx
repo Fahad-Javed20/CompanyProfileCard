ed9e07f6ea9c7c3623e3364edfea2eb7c0cd60fb
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-10-06 10:58:50 +0500

    roundel rectangle corner

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,13 +1,13 @@
 const CompanyCard = () => {
   return (
     <div className="h-screen flex items-center justify-center ">
-      <div className="bg-purple-800 h-120 w-220 flex">
+      <div className="bg-purple-800 h-120 w-220 flex rounded-2xl">
         <div className="w-1/2 flex flex-col justify-between items-start pl-10">
           <div className="h-1/3 pt-10 font-medium text-2xl ">Super Protech Ltd </div>
           <div className="h-1/3 flex flex-col gap-2">
-            <p className="text-white font-bold text-5xl">Company</p>
-            <p className="text-white font-bold text-5xl">Town Hall</p>
-            <p className="text-white text-2xl">January 8,2015</p>
+            <p className="text-white font-bold text-5xl pl-3 ">Company</p>
+            <p className="text-white font-bold text-5xl pl-3">Town Hall</p>
+            <p className="text-white text-2xl pl-3">January 8,2015</p>
           </div>
           <div className="h-1/3">
             <button className="bg-white text-purple-800 px-16 py-1 rounded-sm mt-16">WATCH FROM HERE</button>
